2091b92f7eb3a06c337cef830aee8c99da0d92d6
PCIDIOM <77504471+GamerRPG123@users.noreply.github.com>
2022-07-17 21:07:05 +0100

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,4 @@
-------------------------------------
---- Police Backup, Made by FAXES ---
-------------------------------------
+-- EDITED BY GAMERRPG
 
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
 
